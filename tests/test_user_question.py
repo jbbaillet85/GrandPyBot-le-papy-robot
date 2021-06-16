67e7d0330ff8f0d6ff48e9e6ddd8fac2a0a2a317
jbbaillet <jbbaillet.dev@gmail.com>
2021-06-16 09:20:34 +0200

    test user_question vert

--- tests/test_user_question.py
+++ tests/test_user_question.py
@@ -8,14 +8,13 @@ from grandPyBotApp.user_question import UserQuestion
 
 
 userQuestion = UserQuestion("Salut GrandPy! Est-ce que tu connais l'adresse d'Openclassrooms?")
+userQuestion1 = UserQuestion("où est Openclassrooms")
 
 def test_form_user_question():
     assert userQuestion.form == "Salut GrandPy! Est-ce que tu connais l'adresse d'Openclassrooms?"
 
-def test_parser_user_question():
-    assert userQuestion.pars == "Salut GrandPy connais l'adresse d'Openclassrooms"
+def test_parser_user_question1():
+    assert userQuestion.pars == "Salut GrandPy connais adresse Openclassrooms"
 
-userQuestion1 = UserQuestion("où est Openclassrooms")
-
-def test_parser_user_question():
+def test_parser_user_question2():
     assert userQuestion1.pars == "Openclassrooms"
