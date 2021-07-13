3838d17566bcf61aa1ee6342a52896044c856fef
jbbaillet <jbbaillet.dev@gmail.com>
2021-07-13 17:08:46 +0200

    debut de ajax

--- grandPyBotApp/static/script.js
+++ grandPyBotApp/static/script.js
@@ -1,18 +1,15 @@
-document.getElementById("user_question").addEventListener("submit", function(e){
-    e.preventDefault();
-
-    var data = new FormData(document)
-    console.log(data)
-
-    var httpRequest = new XMLHttpRequest();
-
-    httpRequest.onreadystatechange = function() {
-        console.log(httpRequest)
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {console.log(httpRequest.response);}
-            else{alert("Une erreure est surevenus")}
-
-    httpRequest.open('POST', "../view.py", true);
-    //httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    httpRequest.send(data);
-    return false}}});
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+      $.ajax({
+         data : {
+            userQuestion : $('#userQuestion').val(),
+                },
+            type : 'GET',
+            url : '/data'
+           })
+       .done(function(data) {
+         $('#data').text(data.output).show();
+     });
+     event.preventDefault();
+     });
+});
--- grandPyBotApp/views.py
+++ grandPyBotApp/views.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 # coding utf-8
 
+import json
+
 from flask import Flask, render_template, request
 
 from grandPyBotApp.user_question import UserQuestion
@@ -16,15 +18,33 @@ def index():
     Returns:
         [type]: [description]
     """
-    user_question_form = UserQuestion(str(request.args.get("user_question")))
+    user_question_form = UserQuestion(str(request.args.get("userQuestion")))
     adress = ApiMap(user_question_form.pars)
     history = ApiWikiMedia(adress.city)
     return render_template("index.html",
                             map = adress.imgMap,
                             adress = f"L'adresse de {user_question_form.pars} est {adress.adress}",
                             history = f"Au fait tu savais que {history.history}",
-                            user_question_dialogue = user_question_form.form)
+                            user_question_dialogue = user_question_form.form,
+                            )
 
+
+@app.route('/data', methods=['get', 'post'])
+def data():
+    """[summary]
+
+    Returns:
+        [type]: [description]
+    """
+    user_question_form = UserQuestion(str(request.args.get("userQuestion")))
+    adress = ApiMap(user_question_form.pars)
+    history = ApiWikiMedia(adress.city)
+    data = {"userQuestionDialogue":user_question_form.form, 
+                   "adress": adress.adress,
+                   "map": adress.imgMap,
+                   "history": history.history}
+    return render_template("data.html",
+                            data = json.dumps(data))
 
 if __name__ == "__main__":
     app.run()
