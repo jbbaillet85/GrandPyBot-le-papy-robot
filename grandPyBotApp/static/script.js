54bf3cbb6d6e0256ef620257611825779360dfe4
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2021-07-27 20:14:48 +0200

    ajout du loader

--- grandPyBotApp/static/script.js
+++ grandPyBotApp/static/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+    $(document).ajaxStart(function(){
+        $("#wait").css("display", "block");
+      });
+      $(document).ajaxComplete(function(){
+        $("#wait").css("display", "none");
+      });
     $('form').on('submit', function (event) {
         event.preventDefault();
         $.ajax({
